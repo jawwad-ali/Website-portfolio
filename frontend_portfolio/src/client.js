bde9bd45c685291d8340d3feca7d753794dcc9d7
jawwad-ali <alijawwad001@gmail.com>
2022-03-02 20:25:07 +0500

    final changes

--- frontend_portfolio/src/client.js
+++ frontend_portfolio/src/client.js
@@ -2,12 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "egb4d8up",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production", 
   apiVersion: "2022-02-01",
-  useCdn: true, 
-  token:
-    "skKLNG8Leohd6GMut15a8thLh7rkexnE5gPmccXURioeWQqDXgG5QDXj8Z2gyzo26zkFTnvZ9yaVRAX25oUbn8AkSDABF4bKmG1EZl8kKn26Hmz3COzzruCHNlUSJ0h1FWCz9t9xV5jqYgxfF9Spl6C1FDtZkW6wDEHuRNo9H9s47zmtL9zk",
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
